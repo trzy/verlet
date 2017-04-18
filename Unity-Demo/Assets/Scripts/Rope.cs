e843516064ff1a60157c2b0883d56e6b5492718f
trzy <trzy@u.washington.edu>
2017-04-18 00:16:19 -0400

    Added Unity demo using line renderer for now

---
+++ Unity-Demo/Assets/Scripts/Rope.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope: MonoBehaviour
+{
+  public float ropeLength = 1;
+  public int numSegments = 10;
+  private Verlet.System m_verlet;
+  private List<Verlet.IBody> m_bodies;
+  private GameObject m_lineObject;
+  private LineRenderer m_line;
+
+  private void Update()
+  {
+    m_verlet.Update(Time.deltaTime);
+    for (int i = 0; i < m_bodies.Count; i++)
+    {
+      m_line.SetPosition(i, m_bodies[i].position);
+    }
+  }
+
+  private void Awake()
+  {
+    m_verlet = new Verlet.System();
+    m_bodies = new List<Verlet.IBody>();
+
+    // Construct rope with anchor assumed to be at pivot of this object
+    Verlet.Anchor anchor = new Verlet.Anchor(transform);
+    m_verlet.AddBody(anchor);
+    m_bodies.Add(anchor);
+    float segmentLength = ropeLength / numSegments;
+    for (int i = 1; i < numSegments; i++)
+    {
+      Verlet.PointMass point = new Verlet.PointMass(transform.position - segmentLength * transform.up * i, 1);
+      Verlet.IBody lastPoint = m_bodies[m_bodies.Count - 1];
+      Verlet.IConstraint link = new Verlet.LinkConstraint(lastPoint, point, (point.position - lastPoint.position).magnitude, 1f);
+      point.AddConstraint(link);
+      point.AddForce(new Vector3(0, -9.8f * point.mass, 0));
+      m_verlet.AddBody(point);
+      m_bodies.Add(point);
+    }
+
+    // Debug line renderer
+    m_lineObject = new GameObject("line");
+    m_line = m_lineObject.AddComponent<LineRenderer>();
+    m_line.startWidth = .01f;
+    m_line.endWidth = .01f;
+    m_line.startColor = Color.red;
+    m_line.endColor = Color.red;
+    m_line.positionCount = numSegments;
+  }
+}
---
+++ Unity-Demo/Assets/Scripts/Verlet.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Verlet
+{
+  public interface IConstraint
+  {
+    void Solve();
+  }
+
+  public interface IBody
+  {
+    float mass { get; set; }
+    Vector3 position { get; set; }
+    void Update(float deltaTime);
+    void AddForce(Vector3 force);
+    void AddConstraint(IConstraint constraint);
+  }
+
+  public class LinkConstraint: IConstraint
+  {
+    private IBody m_body1;
+    private IBody m_body2;
+    private float m_maxLength;
+    private float m_stiffness;
+
+    public void Solve()
+    {
+      Vector3 delta = m_body1.position - m_body2.position;
+      float distance = delta.magnitude;
+      Vector3 dir = delta / distance;
+      float p1 = m_body2.mass / (m_body1.mass + m_body2.mass);
+      float p2 = m_body1.mass / (m_body1.mass + m_body2.mass);
+      float adjustment1 = m_stiffness * p1 * (distance - m_maxLength);
+      float adjustment2 = m_stiffness * p2 * (distance - m_maxLength);
+      m_body1.position = m_body1.position - adjustment1 * dir;
+      m_body2.position = m_body2.position + adjustment2 * dir;
+    }
+
+    public LinkConstraint(IBody body1, IBody body2, float length, float stiffness = 1)
+    {
+      m_body1 = body1;
+      m_body2 = body2;
+      m_maxLength = length;
+      m_stiffness = stiffness;
+    }
+  }
+
+  public class Anchor: IBody
+  {
+    private Transform m_transform;
+    
+    public float mass
+    {
+      //TODO: mass should be infinite but we need to rework PointMass solver to use inverted masses in order for that to work
+      get { return 1e9f; }
+      set { }
+    }
+
+    public Vector3 position
+    {
+      get { return m_transform.position; }
+      set { }
+    }
+
+    public void Update(float deltaTime)
+    {
+    }
+
+    public void AddForce(Vector3 force)
+    {
+    }
+
+    public void AddConstraint(IConstraint constraint)
+    {
+    }
+
+    public Anchor(Transform anchoredTo)
+    {
+      m_transform = anchoredTo;
+      position = anchoredTo.position;
+    }
+  }
+
+  public class PointMass: IBody
+  {
+    private Vector3 m_position;
+    private Vector3 m_lastPosition;
+    private Vector3 m_acceleration = Vector3.zero;
+    private float m_mass;
+    private List<IConstraint> m_constraints;
+
+    public float mass
+    {
+      get { return m_mass; }
+      set { m_mass = value; }
+    }
+
+    public Vector3 position
+    {
+      get { return m_position; }
+      set { m_position = value; }
+    }
+
+    public void Update(float deltaTime)
+    {
+      float deltaTime2 = deltaTime * deltaTime;
+      Vector3 nextPosition = m_position + (m_position - m_lastPosition) + m_acceleration * deltaTime2;
+      m_lastPosition = m_position;
+      m_position = nextPosition;
+      foreach (IConstraint constraint in m_constraints)
+      {
+        constraint.Solve();
+      }
+    }
+
+    public void AddForce(Vector3 force)
+    {
+      m_acceleration += force / mass;
+    }
+
+    public void AddConstraint(IConstraint constraint)
+    {
+      m_constraints.Add(constraint);
+    }
+
+    public PointMass(Vector3 p, float m)
+    {
+      position = p;
+      m_lastPosition = p;
+      mass = m;
+      m_constraints = new List<IConstraint>();
+    }
+  }
+
+  public class System
+  {
+    private List<IBody> m_bodies;
+    private float m_timeLeftOver = 0;
+
+    public void Update(float timeSinceLastCalled)
+    {
+      float timeStep = 1 / 120f;
+      float deltaTime = timeSinceLastCalled + m_timeLeftOver;
+      int numWholeSteps = Mathf.FloorToInt(deltaTime / timeStep);
+      m_timeLeftOver = deltaTime - numWholeSteps * timeStep;
+      for (int step = 0; step < numWholeSteps; step++)
+      {
+        foreach (IBody body in m_bodies)
+        {
+          body.Update(timeStep);
+        }
+      }
+    }
+
+    public void AddBody(IBody body)
+    {
+      m_bodies.Add(body);
+    }
+
+    public System()
+    {
+      m_bodies = new List<IBody>();
+    }
+  }
+}

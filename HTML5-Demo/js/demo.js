c6c3dea2bf166950e603a0fce7bb0700fab627bc
trzy <trzy@u.washington.edu>
2017-04-20 22:26:35 -0400

    Added cloth

--- HTML5-Demo/js/demo.js
+++ HTML5-Demo/js/demo.js
@@ -1,10 +1,12 @@
 /*
  * demo.js
  * Bart Trzynadlowski
- * 2017.04.16
+ * 2017.04.20
  *
  * Verlet integrator engine demo set up.
  */
+ 
+var g_gravity = 1200;
 
 function CreateRope(x, y, length, numSegments)
 {
@@ -18,16 +20,60 @@ function CreateRope(x, y, length, numSegments)
     var lastPt = segments[segments.length - 1];
     var pt = new PointMass(x + (i + 1) * segmentLength, y, 1);
     var link = new LinkConstraint(lastPt, pt, segmentLength, 1);
-    pt.AddForce(0, -9.8 * pt.mass);
+    pt.AddForce(0, -g_gravity * pt.mass);
     pt.AddConstraint(link);
     VerletAddObject(pt);
     segments.push(pt);
   }
   
   // Make last link heavy
-  segments[segments.length - 1].mass = 1000;
+  segments[segments.length - 1].mass = 10;
   
   VerletAddObject(anchor);
+}
+
+function CreateFabric(x, y, width, height, numSegmentsX, numSegmentsY)
+{
+  var stiffness = 1;
+  var segmentWidth = width / numSegmentsX;
+  var segmentHeight = height / numSegmentsY;
+  var points = [];
+  for (var i = 0; i < numSegmentsY; i++)
+  {
+    points[i] = [];
+    for (var j = 0; j < numSegmentsX; j++)
+    {
+      var pt = new PointMass(x + j * segmentWidth, y - i * segmentHeight, 1);
+      if (j > 0)
+      {
+        var leftPt = points[i][points[i].length - 1];
+        var link = new LinkConstraint(leftPt, pt, segmentWidth, stiffness);
+        pt.AddConstraint(link);
+      }
+      if (i == 0)
+      {
+        // Top row is pinned
+        pt.AddConstraint(new PinConstraint(pt, pt.x, pt.y));
+      }
+      else
+      {
+        var topPt = points[i - 1][j];
+        var link = new LinkConstraint(topPt, pt, segmentHeight, stiffness);
+        pt.AddConstraint(link);
+      }
+      points[i].push(pt);
+      VerletAddObject(pt);
+    }
+  }
+
+  // Add gravity
+  for (var i = 1; i < points.length; i++)
+  {
+    for (var j = 0; j < points[i].length; j++)
+    {
+      points[i][j].AddForce(0, -g_gravity * points[i][j].mass);
+    }
+  }
 }
 
 var g_highlightedObject;
@@ -89,6 +135,7 @@ function Demo()
   $("#Viewport").mousedown(OnMouseDown);
   $("#Viewport").mouseup(OnMouseUp);
   var canvas = document.getElementById("Viewport");
-  CreateRope(canvas.width / 2, canvas.height / 2, 300, 10);
+  CreateRope(canvas.width / 4, canvas.height * 0.74, 300, 30);
+  CreateFabric(canvas.width / 2, canvas.height * 0.80, 500, 400, 30, 20);
   VerletStart(OnUpdateComplete);
 }  
